7d029fb64941975b047f8917917f3e59b2ccfbf4
Frizlab <fload@me.com>
2015-06-16 17:40:45 +0200

    New devices integrated

--- TestEDDeviceHardware/EDDeviceHardware/EDDeviceHardware.h
+++ TestEDDeviceHardware/EDDeviceHardware/EDDeviceHardware.h
@@ -8,35 +8,68 @@
 
 #import <Foundation/Foundation.h>
 
-@interface EDDeviceHardware : NSObject
 
+
+#pragma mark - Defining Friendly iOS Devices names
 // -- iPhones
-extern NSString* const kEDDHiPhone1G;
+extern NSString* const kEDDHiPhone;
 extern NSString* const kEDDHiPhone3G;
 extern NSString* const kEDDHiPhone3GS;
-extern NSString* const kEDDHiPhone4;
-extern NSString* const kEDDHVerizoniPhone4;
+extern NSString* const kEDDHiPhone4GSM;
+extern NSString* const kEDDHiPhone4CDMA;
 extern NSString* const kEDDHiPhone4S;
-extern NSString* const kEDDHiPhone5;
+extern NSString* const kEDDHiPhone5GSM;
+extern NSString* const kEDDHiPhone5CDMA;
+extern NSString* const kEDDHiPhone5C;
+extern NSString* const kEDDHiPhone5S;
+extern NSString* const kEDDHiPhone6;
+extern NSString* const kEDDHiPhone6Plus;
 
 // -- iPods
-extern NSString* const kEDDHiPodTouch1G;
-extern NSString* const kEDDHiPodTouch2G;
-extern NSString* const kEDDHiPodTouch3G;
-extern NSString* const kEDDHiPodTouch4G;
+extern NSString* const kEDDHiPodTouch;
+extern NSString* const kEDDHiPodTouch2;
+extern NSString* const kEDDHiPodTouch3;
+extern NSString* const kEDDHiPodTouch4;
+extern NSString* const kEDDHiPodTouch5;
 
 // -- iPads
 extern NSString* const kEDDHiPad;
 extern NSString* const kEDDHiPad2WiFi;
 extern NSString* const kEDDHiPad2GSM;
 extern NSString* const kEDDHiPad2CDMA;
+extern NSString* const kEDDHiPad3WiFi;
+extern NSString* const kEDDHiPad3ATT;
+extern NSString* const kEDDHiPad3Verizon;
+extern NSString* const kEDDHiPad4WiFi;
+extern NSString* const kEDDHiPad4ATT;
+extern NSString* const kEDDHiPad4Verizon;
+extern NSString* const kEDDHiPadAirWiFi;
+extern NSString* const kEDDHiPadAirCellular;
+extern NSString* const kEDDHiPadAirTDLTE;
+extern NSString* const kEDDHiPadAir2WiFi;
+extern NSString* const kEDDHiPadAir2Cellular;
+
+// -- iPad Minis
+extern NSString* const kEDDHiPadMiniWiFi;
+extern NSString* const kEDDHiPadMiniATT;
+extern NSString* const kEDDHiPadMiniVerizon;
+extern NSString* const kEDDHiPadMini2Wifi;
+extern NSString* const kEDDHiPadMini2Cellular;
+extern NSString* const kEDDHiPadMini2China;
+extern NSString* const kEDDHiPadMini3Wifi;
+extern NSString* const kEDDHiPadMini3Cellular;
+extern NSString* const kEDDHiPadMini3China;
 
 // -- simulators
 extern NSString* const kSimulatorIntelI386;
 extern NSString* const kSimulatorIntelX86_64;
 
-// -- unknown or Super-New
+// -- unknown or newer than implementation time...
 extern NSString* const kUnknownPlatform;
+
+
+
+@interface EDDeviceHardware : NSObject
 
 @property(nonatomic, readonly) NSString* platformName;
 @property(nonatomic, readonly) NSString* platformCode;
--- TestEDDeviceHardware/EDDeviceHardware/EDDeviceHardware.m
+++ TestEDDeviceHardware/EDDeviceHardware/EDDeviceHardware.m
@@ -10,55 +10,66 @@
 #include <sys/types.h>
 #include <sys/sysctl.h>
 
-// --
-#pragma mark - Defining platform system names
-// -- iPhones
-#define K_PLATFORM_IPHONE_1_1 @"iPhone1,1"
-#define K_PLATFORM_IPHONE_1_2 @"iPhone1,2"
-#define K_PLATFORM_IPHONE_2_1 @"iPhone2,1"
-#define K_PLATFORM_IPHONE_3_1 @"iPhone3,1"
-#define K_PLATFORM_IPHONE_3_3 @"iPhone3,3"
-#define K_PLATFORM_IPHONE_4_1 @"iPhone4,1"
-#define K_PLATFORM_IPHONE_5_1 @"iPhone5,1"
-// -- iPods
-#define K_PLATFORM_IPOD_1_1 @"iPod1,1"
-#define K_PLATFORM_IPOD_2_1 @"iPod2,1"
-#define K_PLATFORM_IPOD_3_1 @"iPod3,1"
-#define K_PLATFORM_IPOD_4_1 @"iPod4,1"
-// -- iPads
-#define K_PLATFORM_IPAD_1_1 @"iPad1,1"
-#define K_PLATFORM_IPAD_2_1 @"iPad2,1"
-#define K_PLATFORM_IPAD_2_2 @"iPad2,2"
-#define K_PLATFORM_IPAD_2_3 @"iPad2,3"
-// -- Simulators
-#define K_PLATFORM_I386 @"i386"
-#define K_PLATFORM_X86_64 @"x86_64"
 
-// --
-#pragma mark - Defining user-friendly device names
+
+#pragma mark - Defining Friendly iOS Devices names
 // -- iPhones
-#define K_DEVICE_IPHONE_1_1 @"iPhone 1G"
-#define K_DEVICE_IPHONE_1_2 @"iPhone 3G"
-#define K_DEVICE_IPHONE_2_1 @"iPhone 3GS"
-#define K_DEVICE_IPHONE_3_1 @"iPhone 4"
-#define K_DEVICE_IPHONE_3_3 @"Verizon iPhone 4"
-#define K_DEVICE_IPHONE_4_1 @"iPhone 4S"
-#define K_DEVICE_IPHONE_5_1 @"iPhone 5"
+NSString* const kEDDHiPhone         = @"iPhone (EDGE)";
+NSString* const kEDDHiPhone3G       = @"iPhone 3G";
+NSString* const kEDDHiPhone3GS      = @"iPhone 3GS";
+NSString* const kEDDHiPhone4GSM     = @"iPhone 4 (GSM)";
+NSString* const kEDDHiPhone4CDMA    = @"iPhone 4 (CDMA)";
+NSString* const kEDDHiPhone4S       = @"iPhone 4S";
+NSString* const kEDDHiPhone5GSM     = @"iPhone 5 (GSM)";
+NSString* const kEDDHiPhone5CDMA    = @"iPhone 5 (CDMA)";
+NSString* const kEDDHiPhone5C       = @"iPhone 5C";
+NSString* const kEDDHiPhone5S       = @"iPhone 5S";
+NSString* const kEDDHiPhone6        = @"iPhone 6";
+NSString* const kEDDHiPhone6Plus    = @"iPhone 6 Plus";
+
 // -- iPods
-#define K_DEVICE_IPOD_1_1 @"iPod Touch 1G"
-#define K_DEVICE_IPOD_2_1 @"iPod Touch 2G"
-#define K_DEVICE_IPOD_3_1 @"iPod Touch 3G"
-#define K_DEVICE_IPOD_4_1 @"iPod Touch 4G"
+NSString* const kEDDHiPodTouch  = @"iPod Touch";
+NSString* const kEDDHiPodTouch2 = @"iPod Touch 2";
+NSString* const kEDDHiPodTouch3 = @"iPod Touch 3";
+NSString* const kEDDHiPodTouch4 = @"iPod Touch 4";
+NSString* const kEDDHiPodTouch5 = @"iPod Touch 5";
+
 // -- iPads
-#define K_DEVICE_IPAD_1_1 @"iPad"
-#define K_DEVICE_IPAD_2_1 @"iPad 2 (WiFi)"
-#define K_DEVICE_IPAD_2_2 @"iPad 2 (GSM)"
-#define K_DEVICE_IPAD_2_3 @"iPad 2 (CDMA)"
-// -- Simulators
-#define K_DEVICE_I386 @"Simulator (Intel i386)"
-#define K_DEVICE_X86_64 @"Simulator (Intel x86_64)"
-// -- Unknown device
-#define K_DEVICE_UNKNOWN @"Unknown"
+NSString* const kEDDHiPad             = @"iPad";
+NSString* const kEDDHiPad2WiFi        = @"iPad 2 WiFi";
+NSString* const kEDDHiPad2GSM         = @"iPad 2 GSM";
+NSString* const kEDDHiPad2CDMA        = @"iPad 2 CDMA";
+NSString* const kEDDHiPad3WiFi        = @"iPad 3 WiFi";
+NSString* const kEDDHiPad3ATT         = @"iPad 3 AT&T";
+NSString* const kEDDHiPad3Verizon     = @"iPad 3 Verizon";
+NSString* const kEDDHiPad4WiFi        = @"iPad 4 WiFi";
+NSString* const kEDDHiPad4ATT         = @"iPad 4 AT&T";
+NSString* const kEDDHiPad4Verizon     = @"iPad 4 Verizon";
+NSString* const kEDDHiPadAirWiFi      = @"iPad Air WiFi";
+NSString* const kEDDHiPadAirCellular  = @"iPad Air Cellular";
+NSString* const kEDDHiPadAirTDLTE     = @"iPad Air TD-LTE";
+NSString* const kEDDHiPadAir2WiFi     = @"iPad Air 2 WiFi";
+NSString* const kEDDHiPadAir2Cellular = @"iPad Air 2 Cellular";
+
+// -- iPad Minis
+NSString* const kEDDHiPadMiniWiFi      = @"iPad Mini WiFi";
+NSString* const kEDDHiPadMiniATT       = @"iPad Mini AT&T";
+NSString* const kEDDHiPadMiniVerizon   = @"iPad Mini Verizon";
+NSString* const kEDDHiPadMini2Wifi     = @"iPad Mini 2 Wifi";
+NSString* const kEDDHiPadMini2Cellular = @"iPad Mini 2 Cellular";
+NSString* const kEDDHiPadMini2China    = @"iPad Mini 2 China";
+NSString* const kEDDHiPadMini3Wifi     = @"iPad Mini 3 Wifi";
+NSString* const kEDDHiPadMini3Cellular = @"iPad Mini 3 Cellular";
+NSString* const kEDDHiPadMini3China    = @"iPad Mini 3 China";
+
+// -- simulators
+NSString* const kSimulatorIntelI386 = @"Simulator (Intel i386)";
+NSString* const kSimulatorIntelX86_64 = @"Simulator (Intel x86_64)";
+
+// -- unknown or newer than implementation time...
+NSString* const kUnknownPlatform = @"Unknown";
+
+
 
 @interface EDDeviceHardware ()
 {
@@ -75,34 +86,6 @@
 
 @synthesize platformName = _platformName;
 @synthesize platformCode = _platformCode;
-
-// -- iPhones
-NSString* const kEDDHiPhone1G = K_DEVICE_IPHONE_1_1;
-NSString* const kEDDHiPhone3G = K_DEVICE_IPHONE_1_2;
-NSString* const kEDDHiPhone3GS = K_DEVICE_IPHONE_2_1;
-NSString* const kEDDHiPhone4 = K_DEVICE_IPHONE_3_1;
-NSString* const kEDDHVerizoniPhone4 = K_DEVICE_IPHONE_3_3;
-NSString* const kEDDHiPhone4S = K_DEVICE_IPHONE_4_1;
-NSString* const kEDDHiPhone5 = K_DEVICE_IPHONE_5_1;
-
-// -- iPods
-NSString* const kEDDHiPodTouch1G = K_DEVICE_IPOD_1_1;
-NSString* const kEDDHiPodTouch2G = K_DEVICE_IPOD_2_1;
-NSString* const kEDDHiPodTouch3G = K_DEVICE_IPOD_3_1;
-NSString* const kEDDHiPodTouch4G = K_DEVICE_IPOD_4_1;
-
-// -- iPads
-NSString* const kEDDHiPad = K_DEVICE_IPAD_1_1;
-NSString* const kEDDHiPad2WiFi = K_DEVICE_IPAD_2_1;
-NSString* const kEDDHiPad2GSM = K_DEVICE_IPAD_2_2;
-NSString* const kEDDHiPad2CDMA = K_DEVICE_IPAD_2_3;
-
-// -- simulators
-NSString* const kSimulatorIntelI386 = K_DEVICE_I386;
-NSString* const kSimulatorIntelX86_64 = K_DEVICE_X86_64;
-
-// -- unknown or Super-New
-NSString* const kUnknownPlatform = K_DEVICE_UNKNOWN;
 
 #pragma mark - Singleton allocation
 static EDDeviceHardware* _sharedInstance = nil;
@@ -152,61 +135,64 @@ static EDDeviceHardware* _sharedInstance = nil;
 
 - (NSString*) getPlatformName
 {
-    NSArray* platformNames = [[NSArray alloc] initWithObjects:
-                              K_PLATFORM_IPHONE_1_1,
-                              K_PLATFORM_IPHONE_1_2,
-                              K_PLATFORM_IPHONE_2_1,
-                              K_PLATFORM_IPHONE_3_1,
-                              K_PLATFORM_IPHONE_3_3,
-                              K_PLATFORM_IPHONE_4_1,
-                              K_PLATFORM_IPHONE_5_1,
-                              K_PLATFORM_IPOD_1_1,
-                              K_PLATFORM_IPOD_2_1,
-                              K_PLATFORM_IPOD_3_1,
-                              K_PLATFORM_IPOD_4_1,
-                              K_PLATFORM_IPAD_1_1,
-                              K_PLATFORM_IPAD_2_1,
-                              K_PLATFORM_IPAD_2_2,
-                              K_PLATFORM_IPAD_2_3,
-                              K_PLATFORM_I386,
-                              K_PLATFORM_X86_64,
-                              nil];
+    /* Source: http://www.everymac.com */
+    NSDictionary *platforms = @{/* iPhone */
+                                @"iPhone1,1": kEDDHiPhone,
+                                @"iPhone1,2": kEDDHiPhone3G,  /* Also, iPhone 3G,  China only (no Wi-Fi) */
+                                @"iPhone2,1": kEDDHiPhone3GS, /* Also, iPhone 3GS, China only (no Wi-Fi) */
+                                @"iPhone3,1": kEDDHiPhone4GSM,
+                                @"iPhone3,2": kEDDHiPhone4GSM,  /* Wikipedia has this model, but not found on everymac */
+                                @"iPhone3,3": kEDDHiPhone4CDMA, /* (Verizon, Sprint) */
+                                @"iPhone4,1": kEDDHiPhone4S,    /* GSM and CDMA. Also, iPhone 4S, China only (Wi-Fi replaced by its Chinese equivalent: WAPI) */
+                                @"iPhone5,1": kEDDHiPhone5GSM,  /* Multiple versions of this iPhone exists, each supporting different communication bands; all GSM though */
+                                @"iPhone5,2": kEDDHiPhone5CDMA, /* Also, iPhone 5 CDMA China only (no LTE, but UIM and Wi-Fi replaced by its Chinese equivalent: WAPI) */
+                                @"iPhone5,3": kEDDHiPhone5C,    /* GSM and CDMA */
+                                @"iPhone5,4": kEDDHiPhone5C,    /* GSM and CDMA; other countries than previous model. */
+                                @"iPhone6,1": kEDDHiPhone5S,    /* GSM and CDMA */
+                                @"iPhone6,2": kEDDHiPhone5S,    /* GSM and CDMA; other countries than previous model. */
+                                @"iPhone7,1": kEDDHiPhone6Plus, /* GSM, CDMA & Global */
+                                @"iPhone7,2": kEDDHiPhone6,     /* GSM, CDMA & Global */
+                                /* iPods */
+                                @"iPod1,1": kEDDHiPodTouch,
+                                @"iPod2,1": kEDDHiPodTouch2, /* 2009 & 2010 */
+                                @"iPod3,1": kEDDHiPodTouch3,
+                                @"iPod4,1": kEDDHiPodTouch4, /* 2011 & 2012, with and without FaceTime */
+                                @"iPod5,1": kEDDHiPodTouch5,
+                                /* iPads */
+                                @"iPad1,1": kEDDHiPad, /* With and without 3G/GPS */
+                                @"iPad2,1": kEDDHiPad2WiFi,
+                                @"iPad2,2": kEDDHiPad2GSM,
+                                @"iPad2,3": kEDDHiPad2CDMA,
+                                @"iPad2,4": kEDDHiPad2WiFi, /* Don't know the difference with iPad2,1... */
+                                @"iPad2,5": kEDDHiPadMiniWiFi,
+                                @"iPad2,6": kEDDHiPadMiniATT,
+                                @"iPad2,7": kEDDHiPadMiniVerizon,
+                                @"iPad3,1": kEDDHiPad3WiFi,
+                                @"iPad3,2": kEDDHiPad3Verizon,
+                                @"iPad3,3": kEDDHiPad3ATT,
+                                @"iPad3,4": kEDDHiPad4WiFi,
+                                @"iPad3,5": kEDDHiPad4ATT,
+                                @"iPad3,6": kEDDHiPad4Verizon,
+                                @"iPad4,1": kEDDHiPadAirWiFi,
+                                @"iPad4,2": kEDDHiPadAirCellular,
+                                @"iPad4,3": kEDDHiPadAirTDLTE, /* China */
+                                @"iPad4,4": kEDDHiPadMini2Wifi,
+                                @"iPad4,5": kEDDHiPadMini2Cellular,
+                                @"iPad4,6": kEDDHiPadMini2China,
+                                @"iPad4,7": kEDDHiPadMini3Wifi,
+                                @"iPad4,8": kEDDHiPadMini3Cellular,
+                                @"iPad4,9": kEDDHiPadMini3China,
+                                @"iPad5,3": kEDDHiPadAir2WiFi,
+                                @"iPad5,4": kEDDHiPadAir2Cellular,
+                                /* Simulators */
+                                @"i386":   kSimulatorIntelI386,
+                                @"x86_64": kSimulatorIntelX86_64
+                                };
     
-    NSArray* deviceNames = [[NSArray alloc] initWithObjects:
-                            K_DEVICE_IPHONE_1_1,
-                            K_DEVICE_IPHONE_1_2,
-                            K_DEVICE_IPHONE_2_1,
-                            K_DEVICE_IPHONE_3_1,
-                            K_DEVICE_IPHONE_3_3,
-                            K_DEVICE_IPHONE_4_1,
-                            K_DEVICE_IPHONE_5_1,
-                            K_DEVICE_IPOD_1_1,
-                            K_DEVICE_IPOD_2_1,
-                            K_DEVICE_IPOD_3_1,
-                            K_DEVICE_IPOD_4_1,
-                            K_DEVICE_IPAD_1_1,
-                            K_DEVICE_IPAD_2_1,
-                            K_DEVICE_IPAD_2_2,
-                            K_DEVICE_IPAD_2_3,
-                            K_DEVICE_I386,
-                            K_DEVICE_X86_64,
-                            nil];
+    NSString* platformName = platforms[_platformCode];
+    if (platformName == nil) platformName = kUnknownPlatform;
     
-    NSDictionary* platforms = [[NSDictionary alloc] initWithObjects:deviceNames forKeys:platformNames];
-    [platformNames release];
-    [deviceNames release];
-    
-    NSString* platformName = [platforms objectForKey:_platformCode];
-    
-    
-    if (platformName == nil) {
-        platformName = kUnknownPlatform;
-    }
-    [platformName retain];
-    
-    [platforms release];
-    
-    return [platformName autorelease];
+    return [[platformName retain] autorelease];
 }
 
 #pragma mark - Singleton initialization
